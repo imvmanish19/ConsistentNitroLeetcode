392b1c124d3dd98606ce102f3ac0066e261b74fa
Manish V <imvmanish19@gmail.com>
2023-06-16 18:24:20 +0530

    Problem Link Added

--- src/arrays/easy/MaximumProductDifferenceBetweenTwoPairs.java
+++ src/arrays/easy/MaximumProductDifferenceBetweenTwoPairs.java
@@ -1,5 +1,6 @@
 package arrays.easy;
 
+/** https://leetcode.com/problems/maximum-product-difference-between-two-pairs/ **/
 public class MaximumProductDifferenceBetweenTwoPairs {
     public static void main(String[] args) {
         int[] nums = {5,6,2,7,4};
